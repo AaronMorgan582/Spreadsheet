08b436344115b8532a777583cdd189ab0c82839a
AaronMorgan582 <aaron.st.morgan@gmail.com>
2020-01-15 09:36:11 -0700

    Added functionality for positive integers, as well as operators (*/+-).

---
+++ Spreadsheet/FormulaEvaluator/Evaluator.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace FormulaEvaluator
+{
+    public class Evaluator
+    {
+        public delegate int Lookup(String variable_name);
+
+        public static int Evaluate(String expression, Lookup variableEvaluator)
+        {
+            Stack<String> values = new Stack<String>();
+            Stack<String> operators = new Stack<String>();
+
+            string[] substrings = Regex.Split(expression, "(\\()|(\\))|(-)|(\\+)|(\\*)|(/)");
+
+            for(int index = 0; index < substrings.Length; index++)
+            {
+                String foundCharacter = substrings[index];
+
+                //If the String is a number value:
+                if(int.TryParse(foundCharacter, out int number))
+                {
+                    if(operators.Count != 0 && operators.Peek() == "*")
+                    {
+                        int stackNumber = int.Parse(values.Pop());
+                        //Storing the operator into a value isn't necessary, but it still needs to be removed from the Stack.
+                        operators.Pop();
+                        int result = stackNumber * number;
+                        values.Push(result.ToString());
+                    }
+                    else if(operators.Count != 0 && operators.Peek() == "/")
+                    {
+                        int stackNumber = int.Parse(values.Pop());
+                        operators.Pop();
+                        int result = stackNumber / number;
+                        values.Push(result.ToString());
+                    }
+                    else
+                    {
+                        values.Push(foundCharacter);
+                    }
+                }
+                //Otherwise it is an operator, parentheses, or variable:
+                else
+                {
+                    if(foundCharacter == "+" || foundCharacter == "-")
+                    {
+                        if(operators.Count != 0 && operators.Peek() == "+")
+                        {
+                            int firstStackNumber = int.Parse(values.Pop());
+                            int secondStackNumber = int.Parse(values.Pop());
+                            operators.Pop();
+                            int result = firstStackNumber + secondStackNumber;
+                            values.Push(result.ToString());
+                            operators.Push(foundCharacter);
+                        }
+                        else if (operators.Count != 0 && operators.Peek() == "-")
+                        {
+                            int firstStackNumber = int.Parse(values.Pop());
+                            int secondStackNumber = int.Parse(values.Pop());
+                            operators.Pop();
+                            int result = secondStackNumber - firstStackNumber;
+                            values.Push(result.ToString());
+                            operators.Push(foundCharacter);
+                        }
+                        else
+                        {
+                            operators.Push(foundCharacter);
+                        }
+                    }
+                    else if(foundCharacter == "*" || foundCharacter == "/" || foundCharacter == "(")
+                    {
+                        operators.Push(foundCharacter);
+                    }
+                    else if(foundCharacter == ")")
+                    {
+
+                    }
+                }
+
+                
+            }
+            return int.Parse(values.Pop());
+        }
+    }
+}
--- Spreadsheet/README.txt
+++ Spreadsheet/README.txt
@@ -1,8 +1,8 @@
 Author:     Aaron Morgan
-Partner:    [Partner Name or None]
-Date:       [Date of Creation]
+Partner:    None
+Date:       1/10/20
 Course:     CS 3500, University of Utah, School of Computing
-Assignment: Assignment # - Name of Assignment
+Assignment: Assignment #1 - Formula Evaluator
 Copyright:  CS 3500 and Aaron Morgan - This work may not be copied for use in Academic Coursework.
 
 1. Comments to Evaluators:
@@ -10,7 +10,9 @@ Copyright:  CS 3500 and Aaron Morgan - This work may not be copied for use in Ac
 Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam remaperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
 
 2. Assignment Specific Topics
-Any additional feedback or write up required by the assignment.
+
+    Expected Time to Complete: 10-15 hours.
+    Total Time: 2.5
 
 3. Consulted Peers:
 
@@ -18,6 +20,4 @@ List any peers (or other people) in the class (or outside for that matter) that 
 
 4. References:
 
-    1. Title of page - URL
-    2. Title of page - URL
-    3. Title of page - URL
+    1.
