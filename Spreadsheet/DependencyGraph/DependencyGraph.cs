02a887a941e559b34d488b0a029b4010c89bd885
AaronMorgan582 <aaron.st.morgan@gmail.com>
2020-01-21 19:25:05 -0700

    Dependency Graph and Test file added; ReadMe for DG added. Functions for Size, HasDependents, and HasDependees completed.

---
+++ Spreadsheet/DependencyGraph/DependencyGraph.cs
@@ -0,0 +1,189 @@
+﻿// Skeleton implementation written by Joe Zachary for CS 3500, September 2013.
+// Version 1.1 (Fixed error in comment for RemoveDependency.)
+// Version 1.2 - Daniel Kopta 
+//               (Clarified meaning of dependent and dependee.)
+//               (Clarified names in solution/project structure.)
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpreadsheetUtilities
+{
+    /// <summary>
+    /// (s1,t1) is an ordered pair of strings
+    /// t1 depends on s1; s1 must be evaluated before t1
+    /// 
+    /// A DependencyGraph can be modeled as a set of ordered pairs of strings.  Two ordered pairs
+    /// (s1,t1) and (s2,t2) are considered equal if and only if s1 equals s2 and t1 equals t2.
+    /// Recall that sets never contain duplicates.  If an attempt is made to add an element to a 
+    /// set, and the element is already in the set, the set remains unchanged.
+    /// 
+    /// Given a DependencyGraph DG:
+    /// 
+    ///    (1) If s is a string, the set of all strings t such that (s,t) is in DG is called dependents(s).
+    ///        (The set of things that depend on s)    
+    ///        
+    ///    (2) If s is a string, the set of all strings t such that (t,s) is in DG is called dependees(s).
+    ///        (The set of things that s depends on) 
+    //
+    // For example, suppose DG = {("a", "b"), ("a", "c"), ("b", "d"), ("d", "d")}
+    //     dependents("a") = {"b", "c"}
+    //     dependents("b") = {"d"}
+    //     dependents("c") = {}
+    //     dependents("d") = {"d"}
+    //     dependees("a") = {}
+    //     dependees("b") = {"a"}
+    //     dependees("c") = {"a"}
+    //     dependees("d") = {"b", "d"}
+    /// </summary>
+    public class DependencyGraph
+    {
+        private Dictionary<string, HashSet<string>> dependents; //In this Dictionary, dependees are the key, dependents are the values.
+        private Dictionary<string, HashSet<string>> dependees;  //In this Dictionary, dependents are the key, dependees are the values.
+
+        /// <summary>
+        /// Creates an empty DependencyGraph.
+        /// </summary>
+        public DependencyGraph()
+        {
+            dependents = new Dictionary<string, HashSet<string>>();
+            dependees = new Dictionary<string, HashSet<string>>();
+        }
+
+        /// <summary>
+        /// The number of ordered pairs in the DependencyGraph.
+        /// </summary>
+        public int Size
+        {
+            get 
+            {
+                int total = 0;
+                foreach (KeyValuePair<string, HashSet<string>> dependee in dependents)
+                {
+                    total += dependee.Value.Count;
+                }
+                return total;
+            }
+        }
+
+        /// <summary>
+        /// The size of dependees(s).
+        /// This property is an example of an indexer.  If dg is a DependencyGraph, you would
+        /// invoke it like this:
+        /// dg["a"]
+        /// It should return the size of dependees("a")
+        /// </summary>
+        public int this[string s]
+        {
+            get
+            {
+                HashSet<string> setOfDependees = new HashSet<string>();
+                if(dependees.TryGetValue(s, out setOfDependees))
+                {
+                    return setOfDependees.Count;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Reports whether dependents(s) is non-empty.
+        /// </summary>
+        public bool HasDependents(string s)
+        {
+            HashSet<string> setOfDependents = new HashSet<string>();
+            if(dependents.TryGetValue(s, out setOfDependents))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        /// <summary>
+        /// Reports whether dependees(s) is non-empty.
+        /// </summary>
+        public bool HasDependees(string s)
+        {
+            HashSet<string> setOfDependees = new HashSet<string>();
+            if(dependees.TryGetValue(s, out setOfDependees))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        /// <summary>
+        /// Enumerates dependents(s).
+        /// </summary>
+        public IEnumerable<string> GetDependents(string s)
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// Enumerates dependees(s).
+        /// </summary>
+        public IEnumerable<string> GetDependees(string s)
+        {
+            return null;
+        }
+
+
+        /// <summary>
+        /// <para>Adds the ordered pair (s,t), if it doesn't exist</para>
+        /// 
+        /// <para>This should be thought of as:</para>   
+        /// 
+        ///   t depends on s
+        ///
+        /// </summary>
+        /// <param name="s"> s must be evaluated first. T depends on S</param>
+        /// <param name="t"> t cannot be evaluated until s is</param>        /// 
+        public void AddDependency(string s, string t)
+        {
+        }
+
+
+        /// <summary>
+        /// Removes the ordered pair (s,t), if it exists
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="t"></param>
+        public void RemoveDependency(string s, string t)
+        {
+        }
+
+
+        /// <summary>
+        /// Removes all existing ordered pairs of the form (s,r).  Then, for each
+        /// t in newDependents, adds the ordered pair (s,t).
+        /// </summary>
+        public void ReplaceDependents(string s, IEnumerable<string> newDependents)
+        {
+        }
+
+
+        /// <summary>
+        /// Removes all existing ordered pairs of the form (r,s).  Then, for each 
+        /// t in newDependees, adds the ordered pair (t,s).
+        /// </summary>
+        public void ReplaceDependees(string s, IEnumerable<string> newDependees)
+        {
+        }
+
+    }
+
+}
---
+++ Spreadsheet/DependencyGraph/README.txt
@@ -0,0 +1,28 @@
+Author:     Aaron Morgan
+Partner:    None
+Date:       1/20/2020
+Course:     CS 3500, University of Utah, School of Computing
+Assignment: Assignment #2 - Dependency Graph
+Copyright:  CS 3500 and Aaron Morgan - This work may not be copied for use in Academic Coursework.
+
+1. Comments to Evaluators:
+
+    None at this time.
+
+2. Assignment Specific Topics
+
+    Expected Time to Complete: 15 hours.
+    Time spent on Analysis: 3 hours.
+    Time spent on Implementation:
+    Time spent Debugging:
+    Time spent Testing:
+    Total Time:
+
+3. Consulted Peers:
+
+    1. Alejandro Serrano: Discussion of what data structure to use, as well as how to determine the size of the Dependency Graph.
+
+4. References:
+
+    1. StackOverflow - Identify if a string is a number https://stackoverflow.com/questions/894263/identify-if-a-string-is-a-number
+    2. StackOverflow - C# Regex: Checking for “a-z” and “A-Z” https://stackoverflow.com/questions/6017778/c-sharp-regex-checking-for-a-z-and-a-z
